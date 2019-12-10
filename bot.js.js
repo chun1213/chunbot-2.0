a0e65626cc147b74e591de2b4128024bde1de6ad
chun1213 <53665251+chun1213@users.noreply.github.com>
2019-12-09 22:59:03 -0500

    Update bot.js.js

--- bot.js.js
+++ bot.js.js
@@ -73,7 +73,7 @@ bot.on("message", async message => {
 	}
 	else if (mess.includes("chunbot threaten")&& x==0){
 		var mention= message.mentions.users.first();
-		mention.send("WHERE MY BEANS FAM?");
+		mention.send("WHERE MY BEANS BOI?");
 	
 	
 	}
