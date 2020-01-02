91cb472b37ff808c221cc1d376525b187f54a7e8
chun1213 <53665251+chun1213@users.noreply.github.com>
2020-01-01 21:21:11 -0500

    Update bot.js.js

--- bot.js.js
+++ bot.js.js
@@ -264,7 +264,7 @@ bot.on("message", async message => {
 			}
 		}
 	}
-
+});
 function shuffleArray(array) {
     for (var i = array.length - 1; i > 0; i--) {
         var j = Math.floor(Math.random() * (i + 1));
