d36fa968e1c52e2d51a6c7ea10f4c1c15bbab987
chun1213 <53665251+chun1213@users.noreply.github.com>
2020-01-01 21:42:53 -0500

    Update bot.js.js

--- bot.js.js
+++ bot.js.js
@@ -256,7 +256,7 @@ bot.on("message", async message => {
 	
 	if (mess.includes("-list")){
 	   var playliststr= playlist.toString();
-	   playliststr.replace(",", " ")
+	   playliststr.replace(",", " ");
 	   message.channel.send(playliststr);
 	}
 	
